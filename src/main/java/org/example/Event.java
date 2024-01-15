96153789c849a5633d756a16ccd38e2b0a594a56
hamidaya <info@ayachi.com>
2024-01-15 21:55:16 +0100

    git structure

---
+++ src/main/java/org/example/Event.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Event implements IEevent {
+
+    public void test1() {
+        System.out.println("test test");
+    }
+}
---
+++ src/main/java/org/example/IEevent.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface IEevent {
+        void test1(); // interface method (does not have a body)
+}
+
+
