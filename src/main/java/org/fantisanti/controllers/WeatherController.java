5f0122be8cef2cfa8836e28463fd930c96b12f0e
pa38ny <hamid.ayachi@ing.com>
2024-01-24 18:42:43 +0100

    api created

--- src/main/java/org/fantisanti/controllers/WeatherController.java
+++ src/main/java/org/fantisanti/controllers/WeatherController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
 
     @GetMapping("/api/weather/current")
     public String fanti() {
         return "Zonnig in Amsterdam";
+    } {
     }
-
     // Nieuwe /api/greeting met een naam parameter
     @GetMapping("/api/weather/current/{city}")
     public String greetingWithName(@PathVariable String city) {
@@ -21,6 +24,8 @@ public class WeatherController {
     @GetMapping("/api/weather/current/coordinates")
     public String weather(@RequestParam double lat, @RequestParam double lon) {
         return "Het regent in " + lat +" " + lon;
+
     }
 }
+
 
