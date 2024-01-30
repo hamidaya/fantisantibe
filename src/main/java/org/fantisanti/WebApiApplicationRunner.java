24754f8f722989d36c2a59317fd730da6bbed992
hamidaya <info@ayachi.com>
2024-01-30 14:15:58 +0100

    config voor de database en controller.

--- src/main/java/org/fantisanti/WebApiApplicationRunner.java
+++ src/main/java/org/fantisanti/WebApiApplicationRunner.java
@@ -2,7 +2,9 @@ package org.fantisanti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class WebApiApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(WebApiApplicationRunner.class, args);
--- src/main/java/org/fantisanti/eventRegistration/EventRegistrationController.java
+++ src/main/java/org/fantisanti/eventRegistration/EventRegistrationController.java
@@ -1,6 +1,16 @@
 package org.fantisanti.eventRegistration;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class EventRegistrationController {
+    @GetMapping("/event/{eventName}")
+        public String getEventName (@PathVariable String eventName){
+            return eventName;
+        }
+    }
 
 
-}
+
+
+
+
