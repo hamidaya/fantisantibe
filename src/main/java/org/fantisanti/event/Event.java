ece59dfcde069bc493b7534443c3fa4754f7fbbc
pa38ny <hamid.ayachi@ing.com>
2024-02-06 14:30:47 +0100

    structuur veranderd

--- src/main/java/org/fantisanti/event/Event.java
+++ src/main/java/org/fantisanti/event/Event.java
@@ -2,64 +2,74 @@ package org.fantisanti.event;
 import org.fantisanti.interfaces.IEevent;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 @Entity
 public class Event implements IEevent {
-    Long eventID;
-    String eventName;
-    String eventLocation;
-    Date eventDate;
-    Double eventPrice;
-    Integer availableTickets;
 
-    public Long getEventID() {
-        return eventID;
+    @Id
+    private Long eventID;
+    private String eventName;
+    private String eventLocation;
+    private Date eventDate;
+    private Double eventPrice;
+    private Integer availableTickets;
+
+    public Event() {
     }
-    public void setEventID(Long eventID) {
-        this.eventID = eventID;
+
+        public Long getEventID () {
+            return eventID;
+        }
+        public void setEventID (Long eventID){
+            this.eventID = eventID;
+        }
+        public String getEventName () {
+            return eventName;
+        }
+        public void setEventName (String eventName){
+            this.eventName = eventName;
+        }
+        public String getEventLocation () {
+            return eventLocation;
+        }
+        public void setEventLocation (String eventLocation){
+            this.eventLocation = eventLocation;
+        }
+        public Date getEventDate () {
+            return eventDate;
+        }
+        public void setEventDate (Date eventDate){
+            this.eventDate = eventDate;
+        }
+        public Double getEventPrice () {
+            return eventPrice;
+        }
+        public void setEventPrice (Double eventPrice){
+            this.eventPrice = eventPrice;
+        }
+        public Integer getAvailableTickets () {
+            return availableTickets;
+        }
+        public void setAvailableTickets (Integer availableTickets){
+            this.availableTickets = availableTickets;
+        }
+
+        // Assuming IEevent interface has a method named "test1"
+        @Override
+        public void test1 () {
+            System.out.println("Event: test1");
+        }
+        // Constructor to initialize fields
+    public
+        Event(Long eventID, String eventName, String eventLocation, Date eventDate, Double eventPrice, Integer availableTickets)
+        {
+            this.eventID = eventID;
+            this.eventName = eventName;
+            this.eventLocation = eventLocation;
+            this.eventDate = eventDate;
+            this.eventPrice = eventPrice;
+            this.availableTickets = availableTickets;
+        }
     }
-    public String getEventName() {
-        return eventName;
-    }
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-    public String getEventLocation() {
-        return eventLocation;
-    }
-    public void setEventLocation(String eventLocation) {
-        this.eventLocation = eventLocation;
-    }
-    public Date getEventDate() {
-        return eventDate;
-    }
-    public void setEventDate(Date eventDate) {
-        this.eventDate = eventDate;
-    }
-    public Double getEventPrice() {
-        return eventPrice;
-    }
-    public void setEventPrice(Double eventPrice) {
-        this.eventPrice = eventPrice;
-    }
-    public Integer getAvailableTickets() {
-        return availableTickets;
-    }
-    public void setAvailableTickets(Integer availableTickets) {
-        this.availableTickets = availableTickets;
-    }
-    // Assuming IEevent interface has a method named "test1"
-    @Override
-    public void test1() {
-        System.out.println("Event: test1");
-    }
-    // Constructor to initialize fields
-    public Event(Long eventID, String eventName, String eventLocation, Date eventDate, Double eventPrice, Integer availableTickets) {
-        this.eventID = eventID;
-        this.eventName = eventName;
-        this.eventLocation = eventLocation;
-        this.eventDate = eventDate;
-        this.eventPrice = eventPrice;
-        this.availableTickets = availableTickets;
-    }
-}
+
